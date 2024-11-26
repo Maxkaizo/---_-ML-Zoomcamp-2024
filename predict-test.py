2b2ff0a701a93e4fba2d9a6ba187308b784fb4c3
Maxkaizo <jlmartinezol@outlook.com>
2024-11-25 20:46:15 -0600

    modified:   .gitignore 	new file:   __pycache__/ping.cpython-311.pyc 	new file:   __pycache__/predict.cpython-311.pyc 	new file:   predict-test.ipynb 	new file:   predict-test.py 	modified:   predict.py 	modified:   README.md

---
+++ predict-test.py
@@ -0,0 +1,58 @@
+import requests
+
+url = 'http://localhost:9696/predict'
+
+student = {
+  "resident_seq_number": 2,
+  "sex": "male",
+  "age": 26,
+  "school_type": "public",
+  "school_grade": "bachelors",
+  "dropout": 1,
+  "em_hw_projects": False,
+  "em_tests": False,
+  "em_multimedia_evidence": False,
+  "em_class_participation": False,
+  "em_class_work": False,
+  "em_class_attendance": False,
+  "em_other": False,
+  "em_no_evaluation": False,
+  "et_smartphone": False,
+  "et_laptop": False,
+  "et_desktop_pc": False,
+  "et_tablet": False,
+  "et_flat_screen": False,
+  "et_didactic_material": False,
+  "et_other": False,
+  "et_none": False,
+  "hr_mother": False,
+  "hr_father": False,
+  "hr_female_relative": False,
+  "hr_male_relative": False,
+  "hr_female_non_relative": False,
+  "hr_male_non_relative": False,
+  "hr_none": False,
+  "help_hours": 0,
+  "expected_grade": "unknown",
+  "ap_stressed": False,
+  "ap_depressed": False,
+  "ap_academic_desperation": False,
+  "ap_social_difficulty": False,
+  "ap_no_issues": False,
+  "economic_participation": "worked_one_hour",
+  "work_hours": 60,
+  "economic_consequences": "economic_unsustain",
+  "state_number": 23,
+  "period_type": "bimester",
+  "period_number": 6
+}
+
+
+response = requests.post(url, json=student).json()
+
+print(response)
+
+if response['dropout'] == True:
+    print('please reach out with the student')
+else:
+    print('not to worry')
--- predict.py
+++ predict.py
@@ -1,60 +1,28 @@
-## WIP
-
 import pickle
+from flask import Flask
+from flask import request
+from flask import jsonify
 
 input_file = 'model_C=1.0_T=0.7.bin'
 
 with open(input_file, 'rb') as f_in: 
     dv, model = pickle.load(f_in)
 
-student = {
-  "resident_seq_number": 1,
-  "sex": "male",
-  "age": 13,
-  "school_type": "public",
-  "school_grade": "primary",
-  "dropout": 0,
-  "em_hw_projects": True,
-  "em_tests": True,
-  "em_multimedia_evidence": False,
-  "em_class_participation": False,
-  "em_class_work": True,
-  "em_class_attendance": False,
-  "em_other": False,
-  "em_no_evaluation": False,
-  "et_smartphone": True,
-  "et_laptop": False,
-  "et_desktop_pc": False,
-  "et_tablet": False,
-  "et_flat_screen": False,
-  "et_didactic_material": False,
-  "et_other": False,
-  "et_none": False,
-  "hr_mother": True,
-  "hr_father": False,
-  "hr_female_relative": False,
-  "hr_male_relative": False,
-  "hr_female_non_relative": False,
-  "hr_male_non_relative": False,
-  "hr_none": False,
-  "help_hours": 0.0,
-  "expected_grade": "bachelors",
-  "ap_stressed": True,
-  "ap_depressed": False,
-  "ap_academic_desperation": False,
-  "ap_social_difficulty": False,
-  "ap_no_issues": False,
-  "economic_participation": "studying_other",
-  "work_hours": 0.0,
-  "economic_consequences": "no_consequence",
-  "state_number": 24,
-  "period_type": "year",
-  "period_number": 6.0
-}
+app = Flask('dropout')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+  student = request.get_json()  
 
-X = dv.transform([student])
-y_pred = model.predict_proba(X)[0, 1]
+  X = dv.transform([student])
+  y_pred = model.predict_proba(X)[0, 1]
+  dropout = y_pred >= 0.7
 
-print('input:', student)
-print('output:', y_pred.round(4))
+  result= {
+     'dropout_probability' : float(y_pred),
+     'dropout' : bool(dropout)
+  }
+  return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
